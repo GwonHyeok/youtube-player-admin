a39b03002bbbdfdf79d5e37e61b9ba3aae3a3e83
GwonHyeok <me@ghyeok.io>
2018-10-30 01:45:01 +0900

    api endpoint 변경

--- src/network/apiClient.js
+++ src/network/apiClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'http://13.209.50.205',
+  baseURL: 'http://13.209.50.205/api',
   timeout: 5000
 });
 export default instance
